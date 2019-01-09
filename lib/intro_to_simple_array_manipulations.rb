5c1d66f0e433058874e3a3ae576d6702fbd6bc46
Samidh Patel <spatel13456@gmail.com>
2019-01-09 21:36:29 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push (countries_in_west_africa, next_country) 
+  return countries_in_west_africa.push(next_country)
+end
+
+def using_unshift (neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  return neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
+
+def using_pop(great_hits_of_the_nineties)
+  return great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_arya_killed)
+  return chars_arya_killed.pop(2)
+end
+
+def using_shift(im_so_over_this_city)
+  return im_so_over_this_city.shift
+end
+
+def shift_with_args(brands_removed)
+  return brands_removed.shift(2)
+end
+
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using_insert(array, element)
+  return array.insert(4, element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, int)
+  return array.delete_at(2)
+end
